860bb536de78d3b8a14f377c3e1a87df365ccb26
Nevin Gilday <nevingilday@gmail.com>
2024-01-11 23:35:58 -0800

    remove wpm chaser, it is a pain in my butt

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
+const typingAreaContainer = document.getElementById("typing-area-container");
 const typingArea = document.getElementById("typing-area");
+const typingAreaChild = document.getElementById("typing-area-child");
 const blinkingCursor = document.getElementById("blinking-cursor");
 const topLine = document.getElementById("top-line");
+const middleLine = document.getElementById("middle-line");
 const bottomLine = document.getElementById("bottom-line");
+const startNewText = document.getElementById("start-new-text");
 
 let blinkInterval = undefined;
 let timeElapsed = 0;
 let charsTyped = 0;
+let firstCharisXCharTyped = 0;
 let wpmInterval = undefined;
 const charWidth = getCharWidth();
 const wpmText = document.getElementById("wpm-text");
+let goalSpeed = 50; //wpm to type in
+let testReady = true;
+let testOngoing = false;
 
-//add typing text
-let currentTextLine = generateLine();
-let nextTextLine = generateLine();
-let currentTextLetter = 0;
-addTextToDOM();
+currentTextLetter = 0;
+let currentTextLine = "";
+let nextTextLine = "";
+let lastTextLine = "";
 
-//start the blinking cursor
-resetBlinker();
+typingAreaChild.onanimationend = (e) => {
+    if(e.srcElement == typingAreaChild){
+        typingAreaChild.className = "";
+        addTextToDOM(2);
+    }
+}
+
+resetTest();
 
 //listen for keyboard pressses
 document.addEventListener("keypress", (e) => {
-    if(e.key == currentTextLine[currentTextLetter]){
+    if(e.key == currentTextLine[currentTextLetter] && testReady){
         //start wpm counter if it hasn't been started
-        if(wpmInterval == undefined)
+        if(wpmInterval == undefined){
             wpmInterval = setInterval(updateWPM, 100);
-
+            testOngoing = true;
+        }
+            
         charsTyped++;
         currentTextLetter++;
         //update graphics
@@ -35,10 +50,14 @@ document.addEventListener("keypress", (e) => {
 
         //check if line is complete
         if(currentTextLetter == currentTextLine.length){
+            firstCharisXCharTyped += currentTextLine.length;
             currentTextLetter = 0;
+            lastTextLine = currentTextLine;
             currentTextLine = nextTextLine;
             nextTextLine = generateLine();
-            addTextToDOM();
+
+            typingAreaChild.className = "test-animation";
+            addTextToDOM(1);
         }
     }
 });
@@ -66,30 +85,79 @@ function getCharWidth(){
     return width;
 }
 
-function addTextToDOM(){
-    //remove children from TopLine except for "blinking-cursor"
+function addTextToDOM(stage){
+    let line1text = "";
+    let line2text = "";
+    let line3text = "";
+
+    //do correct order of lines
+    //TODO, moving the order of these causes animations to reset
+    if(stage == 0){
+        line1text = currentTextLine;
+        line2text = nextTextLine;
+        line3text = "";
+        typingAreaChild.insertBefore(topLine, typingAreaChild.children[0]);
+        typingAreaChild.insertBefore(middleLine, typingAreaChild.children[1]);
+    }else if(stage == 1){
+        line1text = lastTextLine;
+        line2text = currentTextLine;
+        line3text = nextTextLine;
+        typingAreaChild.insertBefore(topLine, typingAreaChild.children[0]);
+        typingAreaChild.insertBefore(middleLine, typingAreaChild.children[1]);
+    }else if(stage == 2){
+        line1text = nextTextLine;
+        line2text = currentTextLine;
+        line3text = "";
+        typingAreaChild.insertBefore(middleLine, typingAreaChild.children[0]);
+        typingAreaChild.insertBefore(topLine, typingAreaChild.children[1]);
+    }
+    
+
+    //remove children from TopLine except for "blinking-cursor" and line chaser
     for(let i = 0; i < topLine.children.length; i++){
-        if(topLine.children[i].id != "blinking-cursor"){
+        if(topLine.children[i].id != "blinking-cursor" && topLine.children[i].id != "top-line-chaser"){
             topLine.children[i].remove();
             i--;
         }
     }
-
-    //add text to top line
-    for(let i = 0; i < currentTextLine.length; i++){
-        const letter = document.createElement("span");
-        letter.innerHTML = currentTextLine[i];
-        topLine.appendChild(letter);
+    //remove children from middle line except for "blinking-cursor" and line chaser
+    for(let i = 0; i < middleLine.children.length; i++){
+        if(middleLine.children[i].id != "blinking-cursor" && middleLine.children[i].id != "middle-line-chaser"){
+            middleLine.children[i].remove();
+            i--;
+        }
     }
-
     //remove children from bottom line
     bottomLine.innerHTML = '';
 
-    //add text to bottom line
-    for(let i = 0; i < nextTextLine.length; i++){
+    //add text to top line
+    for(let i = 0; i < line1text.length; i++){
         const letter = document.createElement("span");
-        letter.innerHTML = nextTextLine[i];
+        letter.innerHTML = line1text[i];
+        topLine.appendChild(letter);
+    }
+    //add text to middle line
+    for(let i = 0; i < line2text.length; i++){
+        const letter = document.createElement("span");
+        letter.innerHTML = line2text[i];
+        middleLine.appendChild(letter);
+    }
+    //add text to bottom line
+    for(let i = 0; i < line3text.length; i++){
+        const letter = document.createElement("span");
+        letter.innerHTML = line3text[i];
         bottomLine.appendChild(letter);
+    }
+
+    //add blinking cursor to correct line
+    if(stage == 0){
+        topLine.insertBefore(blinkingCursor, topLine.children[currentTextLetter]);
+    }
+    else if (stage == 1){
+        middleLine.insertBefore(blinkingCursor, middleLine.children[currentTextLetter ]);
+    }
+    else if(stage == 2){
+        middleLine.insertBefore(blinkingCursor, middleLine.children[currentTextLetter + 1]);
     }
 }
 
@@ -100,7 +168,7 @@ function generateLine(){
     while(true){
         //get random word
         const word = words[Math.floor(Math.random() * words.length)].toLowerCase();
-        if(line.length + word.length > typingArea.offsetWidth / charWidth)
+        if(line.length + word.length > typingAreaContainer.offsetWidth / charWidth)
             break;
         line += word + " ";
     }
@@ -113,7 +181,75 @@ function generateLine(){
 
 function updateWPM(){
     timeElapsed += .1;
-    const newText = "Words per minute: " + (charsTyped / timeElapsed * 12).toFixed(0);
+    const wpm = (charsTyped / timeElapsed * 12)
+    const newText = "Words per minute: " + wpm.toFixed(0) + " Time: " + (timeElapsed).toFixed(0)+ "s";
     if(newText != wpmText.innerHTML)
-        wpmText.innerHTML = "Words per minute: " + (charsTyped / timeElapsed * 12).toFixed(0);
+        wpmText.innerHTML = newText;
+
+    if(wpm < goalSpeed){
+        endTest();
+    }
+}
+
+function resetTest(){
+    blinkInterval = undefined;
+    timeElapsed = 0;
+    charsTyped = 0;
+    wpmInterval = undefined;
+    testReady = true;
+    firstCharisXCharTyped = 0;
+
+    //add typing text
+    lastTextLine = "";
+    currentTextLine = generateLine();
+    nextTextLine = generateLine();
+    currentTextLetter = 0;
+    addTextToDOM(0);
+
+    //start the blinking cursor
+    resetBlinker();
+
+    //set wpm text
+    wpmText.innerHTML = `Goal: <input style="width: 3rem" type="number" value="${goalSpeed}" min="20" onchange="setGoalSpeed(this)"/> words per minute`;
+
+    //reset text blur
+    typingArea.style.filter = "none";
+
+    //remove start new test text
+    startNewText.style.display = "none";
+
+}
+
+function setGoalSpeed(e){
+    goalSpeed = Math.max(20, parseInt(e.value));
+    document.documentElement.style.setProperty('--chaser-duration', (typingAreaContainer.clientWidth / charWidth) / (goalSpeed / 12) + "s");
+}
+
+function endTest(){
+    clearInterval(wpmInterval);
+    clearInterval(blinkInterval);
+    blinkingCursor.style.backgroundColor = "black";
+    testReady = false;
+    testOngoing = false;
+    typingArea.style.filter = "blur(2px)";
+    wpmText.innerHTML = "Time: " + (timeElapsed).toFixed(1)+ "s";
+        
+    //add click to start new test
+    startNewText.style.display = "block";
+
+    //reset on click or enter
+    document.addEventListener("click", checkForClick);
+    document.addEventListener("keypress", checkForEnter);
+    function checkForClick(e){
+        document.removeEventListener("click", checkForClick);
+        document.removeEventListener("keypress", checkForEnter);
+        resetTest();
+    }
+    function checkForEnter(e){
+        if(e.key == "Enter"){
+            document.removeEventListener("click", checkForClick);
+            document.removeEventListener("keypress", checkForEnter);
+            resetTest();
+        }
+    }
 }
